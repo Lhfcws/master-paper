6f75b3757033ff97d68e697e74dd7f2dc876d977
lhfcws <wenjie@datatub.com>
2016-07-22 18:32:52 +0800

    render!

--- src/main/bin/sysu.sh
+++ src/main/bin/sysu.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-nohup sh run.sh community -uid 1892723783 -noscanuser -nocontent -notag -nodetect -norender > sysu.log &
+nohup sh run.sh community -uid 1892723783 -noscanuser -nocontent -notag > sysu.log &
 #nohup sh run.sh community -uid 1994626454 -noscanuser -nocontent -notag -nocommtag > sysu.log &
 tailf sysu.log
