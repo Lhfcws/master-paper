cf587361d8d46b370221e652a9fff5a06329e76c
lhfcws <wenjie@datatub.com>
2016-08-14 12:29:08 +0800

    only commtag

--- src/main/bin/sysu.sh
+++ src/main/bin/sysu.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-nohup sh run.sh community -uid 1892723783 -noscanuser -nocontent -norender > sysu.log &
+nohup sh run.sh community -uid 1892723783 -noscanuser -nocontent -notag -norender > sysu.log &
 #nohup sh run.sh community -uid 1994626454 -noscanuser -nocontent -notag -nocommtag > sysu.log &
 tailf sysu.log
